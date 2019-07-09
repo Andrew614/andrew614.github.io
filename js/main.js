ef485a976a12f9e2afb3f309f7ce536d421e9bdf
Andrew614 <azhang213@gmail.com>
2019-07-08 21:41:35 -0400

    delete unused js files

--- js/main.js
+++
@@ -1,8 +0,0 @@
-import "./main.css"
-import Fireworks from "fireworks-canvas"
-
-export default () => {
-    const container = document.getElementById("container")
-    const fireworks = new Fireworks(container)
-    fireworks.start()
-}
