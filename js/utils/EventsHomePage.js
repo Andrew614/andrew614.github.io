51672a2788cf8dc38f9285d0935c0485822f8310
Andrew614 <azhang213@gmail.com>
2019-07-14 01:36:54 -0400

    add transition effect upon clicking the home page

--- js/utils/EventsHomePage.js
+++ js/utils/EventsHomePage.js
@@ -1,11 +1,25 @@
 class EventsHomePage {
 
+    homePageTransition() {
+        const home = document.querySelector('.homepage');
+
+        home.addEventListener('click', () => {
+            home.classList.add('transition--end');
+        });
+    }
+
     homePageRedirect() {
         const home = document.querySelector('.homepage');
 
         home.addEventListener('click', () => {
-            window.location.href = 'index.html';
+            setTimeout(function () {
+                window.location.href = 'index.html';
+            }, 1400)
         })
+
+        setTimeout(function () {
+            window.location.href = 'index.html';
+        }, 45000);
 
     }
 }
